92ca3ec6746faad79b8218c1542a9e9d501421b5
Ken Yeck <967483+kenyeck@users.noreply.github.com>
2025-08-01 11:03:02 -0700

    Compose prod images

--- apps/api-gateway/src/index.ts
+++ apps/api-gateway/src/index.ts
@@ -1,4 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config({
+   path: `./.env.${process.env.NODE_ENV ? `${process.env.NODE_ENV}` : 'development'}`
+});
 const app = express();
 app.get('/api/health', (req, res) => res.send('API Gateway is running'));
 const PORT = process.env.PORT || 4001;
--- services/auth-service/src/index.ts
+++ services/auth-service/src/index.ts
@@ -1,4 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config({
+   path: `./.env.${process.env.NODE_ENV ? `${process.env.NODE_ENV}` : 'development'}`
+});
+
 const app = express();
 app.get('/api/health', (req, res) => res.send('Auth Service is running'));
 const PORT = process.env.PORT || 4000;
